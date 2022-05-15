7431c22d062e04507f4bd807b1442dedcdb75c86
hong630 <hongihongi60@gmail.com>
2022-05-15 22:10:49 +0900

    js

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ function test(data){
 
 
 function shareTwitter() {
+    const metas = document.querySelectorAll('meta');
+    metas[6].content = "바뀐건가? 제발 바뀌었길 제발 "
     var sendText = "테스트"; // 전달할 텍스트
     var sendUrl = "https://glittering-truffle-5a92d6.netlify.app/"; // 전달할 URL
     window.open("https://twitter.com/intent/tweet?text=" + sendText + "&url=" + sendUrl);
