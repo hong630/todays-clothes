85bd4b836783a267c12e874b98828a925e0f8cb9
hong630 <hongihongi60@gmail.com>
2022-05-15 22:22:36 +0900

    test

--- index.js
+++ index.js
@@ -10,10 +10,14 @@ function test(data){
     console.log(data)
 }
 
-
-function shareTwitter() {
+window.onload = function (){
     const metas = document.querySelectorAll('meta');
-    metas[6].content = "바뀐건가? 제발 바뀌었길 제발 "
+    metas[3].content = "바뀐 타이틀"
+    metas[4].content = "바뀐 설명"
+    metas[6].content = "바뀐건가? 제발 바뀌었길 제발"
+}
+function shareTwitter() {
+
     var sendText = "테스트"; // 전달할 텍스트
     var sendUrl = "https://glittering-truffle-5a92d6.netlify.app/"; // 전달할 URL
     window.open("https://twitter.com/intent/tweet?text=" + sendText + "&url=" + sendUrl);
