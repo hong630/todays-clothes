ca48baf0ab653c454f8c3580c0a5503d4cab05f7
hong630 <hongihongi60@gmail.com>
2022-05-31 00:05:53 +0900

    폴더 세팅

---
+++ my-app/src/pages/App.js
@@ -0,0 +1,11 @@
+import '../styles/App.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+  return (
+    <div className="App">hello</div>
+  );
+}
+
+export default App;
